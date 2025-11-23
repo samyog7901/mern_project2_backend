2f2f470a444b64d823dbbdf6f20f46236619a3f5
samyog7901 <samyogkhadka247@gmail.com>
2025-11-23 11:39:03 +0545

    fix done

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -14,7 +14,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL!,{
     dialect : 'mysql',
     dialectOptions : {
         ssl: {
-            // require: true,
+            require: true,
             rejectUnauthorized: false
         }
     },
