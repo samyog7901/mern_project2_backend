1dd88080485dba6c03c3a37723d385fe9c3909e7
samyog7901 <samyogkhadka247@gmail.com>
2025-11-30 20:21:00 +0545

    security

--- src/adminSeeder.ts
+++ src/adminSeeder.ts
@@ -5,16 +5,16 @@ import bcrypt from 'bcrypt'
 const adminSeeder = async():Promise<void> =>{
     const [data] = await User.findAll({
         where:{
-            email:"p2admin@gmail.com"
+            email:process.env.ADMIN_EMAIL
 
         }
     })
     if(!data){
             await User.create({
-                email :"p2admin@gmail.com",
-                password : bcrypt.hashSync("ShopNestAdmin321",8),
-                role : "admin",
-                username : 'p2admin'
+                email :process.env.ADMIN_EMAIL,
+                password :bcrypt.hashSync("ShopNestAdmin321",8),
+                role :process.env.ADMIN_ROLE,
+                username :process.env.ADMIN_USERNAME
             })
             console.log("admin credentials seeded successfully")
             
